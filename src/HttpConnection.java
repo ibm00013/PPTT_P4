c5b24bdf8df664e5f93b0c28fa8590295188cc6b
ignab <ignab@Municicle-PC>
2022-01-03 20:10:18 +0100

    Análisis de la estructura básica del servidor

--- src/HttpConnection.java
+++ src/HttpConnection.java
@@ -3,7 +3,7 @@ import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.Socket;
+import java.net.Socket;//Implementamos la clase java.net que utiliza Java para las comunicaciones de red.
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
--- src/MainServer.java
+++ src/MainServer.java
@@ -1,6 +1,6 @@
 
 import java.io.IOException;
-import java.net.InetAddress;
+import java.net.InetAddress;//Implementamos la clase java.net que utiliza Java para las comunicaciones de red.
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
@@ -29,21 +29,21 @@ import java.util.logging.Logger;
  ******************************************************/
 public class MainServer {
     
-    static ServerSocket server=null;
-    static final short TCP_PORT = 80; //Puerto TCP escucha
+    static ServerSocket server=null;//Instanciamos una variable server de la clase ServerSocket.
+    static final short TCP_PORT = 80; //Definimos una constante con el puerto a utilizar en la comunicación TCP.
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         
         try {
-            InetAddress serveraddr = InetAddress.getLocalHost();
-            server = new ServerSocket(TCP_PORT,5,serveraddr);
-            System.out.println("Simple HTTP/1.1. Server waiting in "+serveraddr+" port "+TCP_PORT);
+            InetAddress serveraddr = InetAddress.getLocalHost();//A través del método getLocalHost de la clase InetAdress obtenemos la dirección local del servidor.
+            server = new ServerSocket(TCP_PORT,5,serveraddr);//Creamos el servidor llamando al constructor ServerSocket con los parámetros TCP_PORT para el puerto, '5' para el tamaño de la cola de espera y la dirección con serveraddr.
+            System.out.println("Simple HTTP/1.1. Server waiting in "+serveraddr+" port "+TCP_PORT);//Imprime por consola mensaje de introducción al servidor con dirección y puerto.
             while(true){
-                Socket s = server.accept();
-                HttpConnection conn = new HttpConnection(s);
-                new Thread(conn).start();
+                Socket s = server.accept();//Creamos el socket a través de la conexión entrante con el método accept.
+                HttpConnection conn = new HttpConnection(s);//Creamos una conexión HTTP pasando como argumento el socket recién aceptado.
+                new Thread(conn).start();//Inicializa la multihebra de conexiones. 
             }
         } catch (UnknownHostException ex) {
             Logger.getLogger(MainServer.class.getName()).log(Level.SEVERE, null, ex);
