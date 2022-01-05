7a8e2d340928f8da912e4b0359c7742d8f2d2d0c
ignab <ignab@Municicle-PC>
2022-01-05 16:03:48 +0100

    Soporte del método GET y envío de cabecera Connection: close

--- src/HttpConnection.java
+++ src/HttpConnection.java
@@ -4,6 +4,7 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.InetAddress;
 import java.net.Socket;//Implementamos la clase java.net que utiliza Java para las comunicaciones de red.
 import java.net.URL;
 //import java.net.URLConnection;
@@ -42,13 +43,45 @@ public class HttpConnection implements Runnable {
 		try {
 			System.out.println("Starting new HTTP connection with " + socket.getInetAddress().toString());
 			output = new DataOutputStream(socket.getOutputStream());
-			output.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-			output.flush();
 			BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 			
-			URL urlObj = new URL("https://google.es");
-			HttpURLConnection httpCon = (HttpURLConnection) urlObj.openConnection();
+			URL url = new URL("http://"+InetAddress.getLocalHost().getHostAddress());
+			HttpURLConnection urlcon = (HttpURLConnection) url.openConnection();
 			
+			urlcon.setRequestProperty("Connection", "close");
+			
+			String line = input.readLine();
+			String request = line;
+			if(line.startsWith("GET")) {
+				System.out.println(line);
+				while (!(line=input.readLine()).equals("") && line!=null) {
+					System.out.println("Leído["+line.length()+"]: "+line);
+					//output.write(("ECO " + line + "\r\n").getBytes());
+					//output.flush();
+				}
+				output.write("HTTP/1.1 200 OK\r\n".getBytes());
+		        output.write("Content-Type: text/html;\r\n charset=utf-8;\r\n".getBytes());
+		        output.write("Connection: close; \r\n".getBytes());
+		        output.write("Server: MINISERVER;\r\n".getBytes());
+		            // este linea en blanco marca el final de los headers de la response
+		        output.write("\r\n".getBytes());
+		            // Envía el HTML
+		        output.write("<H1>Bienvenido al Mini Server : 'EL PATRON'</H1>".getBytes());
+		        output.write(("<H2>Request Method->" + request + "</H2>").getBytes());
+		        output.write("<p>Raestoestofufa Raestoestofufa Raestoestofufa Raestoestofufa Raestoestofufa</p>".getBytes());
+		  
+				output.flush();
+			}else {
+				System.out.println("ERROR 405.");
+			}
+			
+			//output.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+			//output.flush();
+			
+			
+			//URL urlObj = new URL("https://google.es");
+			//HttpURLConnection httpCon = (HttpURLConnection) urlObj.openConnection();
+			/*
 			File index = new File("C:\\Users\\ignab\\git\\PPTT_P4\\p4\\index.html");//Creamos una instancia de nuestro fichero index.html
 			//FileOutputStream foutput = new FileOutputStream(index);
 			FileInputStream finput = new FileInputStream(index);
@@ -62,23 +95,17 @@ public class HttpConnection implements Runnable {
 			//String text = "<html><head></head><body><h1>HOLA</h1></body></html>";
 			//foutput.write(text.getBytes());
 		
-			System.out.println("El método es : "+httpCon.getRequestMethod()+ ", la ruta es: "+httpCon.getURL()+ " y la versión del protocolo y código de respuesta es: "+httpCon.getHeaderField(0));
+			//System.out.println("El método es : "+httpCon.getRequestMethod()+ ", la ruta es: "+httpCon.getURL()+ " y la versión del protocolo y código de respuesta es: "+httpCon.getHeaderField(0));
 			
-			if (httpCon.getResponseCode() != HttpURLConnection.HTTP_OK) {// HTTP_OK es 200
+			/*if (httpCon.getResponseCode() != HttpURLConnection.HTTP_OK) {// HTTP_OK es 200
 			System.out.println("El servidor devolvió el código de respuesta" + httpCon.getResponseCode());
 			System.exit(0);
 			}
-			else {
-				String line = input.readLine();
-				while (!(line=input.readLine()).equals("") && line!=null) {
-					System.out.println("Leído["+line.length()+"]: "+line);
-					output.write(("ECO " + line + "\r\n").getBytes());
-					output.flush();
-			}
-				finput.close();
+			else {*/
+				
+				//finput.close();
 			
 			
-			}
 		} catch (IOException ex) {
 			Logger.getLogger(HttpConnection.class.getName()).log(Level.SEVERE, null, ex);
 		} finally {
